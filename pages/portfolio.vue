a583c23af52ee4b6c778037e8bffab7fb141939f
Marine LB <marine.leborgne@live.fr>
2020-06-21 13:24:07 +0200

    add testimonials

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -2,12 +2,21 @@
   <div class="page">
     <div class="content">
       <h2>Portfolio</h2>
-      <p>
-        Tracks gain-matched at true peak max -0.2dBFS.
-      </p>
       <client-only>
+        <p>
+          Tracks gain-matched at true peak max -0.2dBFS.
+        </p>
         <player />
       </client-only>
+    </div>
+
+    <div class="testimonials">
+      <div class="testimonial" v-for="item in testimonials">
+        <h4 class="testimonial__quote">
+          {{ item.quote }}
+        </h4>
+        <span class="testimonial__author">- {{ item.author }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -41,7 +50,91 @@ export default {
         { name: "og:type", content: "website" }
       ]
     };
+  },
+  data() {
+    return {
+      testimonials: [
+        {
+          author: "Advanced Human (Gynoid Audio)",
+          quote: "I LOVE them man, very, very clean once again, and PUNCHY!!!"
+        },
+        {
+          author: "Vinicius Honorio",
+          quote: "The masters are sounding excellent"
+        },
+        {
+          author: "Subjected (PFTA)",
+          quote: "Hey man, the masters sounds ace. Thank you."
+        },
+        {
+          author: "Sandro Galli & DJ OGI",
+          quote: "Love the masters"
+        }
+      ]
+    };
   }
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.testimonials {
+  margin-top: auto;
+}
+.testimonial {
+  margin-bottom: 1rem;
+  background: black;
+  color: white;
+  overflow: hidden;
+  padding: 0.35rem;
+  //white-space: nowrap;
+  text-align: center;
+  cursor: default;
+
+  &:hover {
+    animation: flicker-blue 0.2s infinite;
+  }
+
+  &:nth-child(even) {
+    &:hover {
+      animation: flicker-yellow 0.3s infinite;
+    }
+  }
+}
+.testimonial__quote {
+  display: inline;
+}
+.testimonial__author {
+  font-size: 0.8rem;
+  font-weight: normal;
+}
+
+.testimonials__title {
+  margin-bottom: 1rem;
+}
+
+.hand {
+  transform: rotate(90deg);
+}
+
+@keyframes flicker-blue {
+  0% {
+    background: black;
+  }
+  50% {
+    background: $primary-accent;
+  }
+  100% {
+    background: black;
+  }
+}
+@keyframes flicker-yellow {
+  0% {
+    background: black;
+  }
+  50% {
+    background: $secondary-accent;
+  }
+  100% {
+    background: black;
+  }
+}
+</style>
